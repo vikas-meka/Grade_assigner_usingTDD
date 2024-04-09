f605f25c8715729473bf01803a9dadaff9fcbdf2
vikas-meka <phanindrajlk@gmail.com>
2024-04-09 14:52:32 +0530

    testing A grade

--- grade_assigner.py
+++ grade_assigner.py
@@ -5,3 +5,7 @@ def calculate_grade(marks, highest_marks):
 
     if percentage >= 90:
         return 'S'
+    
+    elif percentage >= 80:
+        return 'A'
+    
--- test_grade_assigner.py
+++ test_grade_assigner.py
@@ -5,3 +5,6 @@ class TestGradeCalculator(unittest.TestCase):
 
     def test_calculate_grade_highest_marks_minus_ten(self):
         self.assertEqual(calculate_grade(90, 100), 'S')  
+    
+    def test_calculate_grade_highest_marks_minus_twenty(self):
+        self.assertEqual(calculate_grade(80, 100), 'A')
