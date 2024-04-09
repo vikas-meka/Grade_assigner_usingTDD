aea5da61d817791070cb413f3aa36f27be7849e4
vikas-meka <phanindrajlk@gmail.com>
2024-04-09 15:27:04 +0530

    testing B grade

--- test_grade_assigner.py
+++ test_grade_assigner.py
@@ -5,6 +5,7 @@ class TestGradeCalculator(unittest.TestCase):
     def test_calculate_grade(self):
         self.assertEqual(calculate_grade(90, 100), 'S') 
         self.assertEqual(calculate_grade(80, 100), 'A')
+        self.assertEqual(calculate_grade(70, 100), 'B')
     
     
 if __name__ == "__main__":
