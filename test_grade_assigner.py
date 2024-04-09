4c23f1b7271f2b87b1c3a43f2b3325efb714f22d
vikas-meka <phanindrajlk@gmail.com>
2024-04-09 14:48:20 +0530

    testing S grade

---
+++ test_grade_assigner.py
@@ -0,0 +1,7 @@
+import unittest
+from grade_assigner import calculate_grade
+
+class TestGradeCalculator(unittest.TestCase):
+
+    def test_calculate_grade_highest_marks_minus_ten(self):
+        self.assertEqual(calculate_grade(90, 100), 'S')  
