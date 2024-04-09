a9e60e7978907fd27155c11a9e3cca8335a2be17
vikas-meka <phanindrajlk@gmail.com>
2024-04-09 15:14:47 +0530

    testing

--- grade_assigner.py
+++ grade_assigner.py
@@ -4,9 +4,10 @@ def calculate_grade(marks, highest_marks):
     percentage = (marks / highest_marks) * 100
 
 
-    if percentage >= 90:
+    if percentage >= 90 and percentage <=100:
         return 'S'
-    
+    else:
+        return 'I'
     
   
 
