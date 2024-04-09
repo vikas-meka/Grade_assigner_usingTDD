361e6a055e54833973f7997905b7f11e05c9be01
vikas-meka <phanindrajlk@gmail.com>
2024-04-09 15:20:32 +0530

    test

--- test_grade_assigner.py
+++ test_grade_assigner.py
@@ -2,11 +2,11 @@ import unittest
 from grade_assigner import calculate_grade
 
 class TestGradeCalculator(unittest.TestCase):
-
     def test_calculate_grade(self):
-        self.assertEqual(calculate_grade(80, 100), 'A') 
+        self.assertEqual(calculate_grade(80, 100), 'A')
         # self.assertEqual(calculate_grade(90, 100), 'S') 
     
     
-        
+if __name__ == "__main__":
+    unittest.main()   
 
